c077d33daf756190a6306e889f0cf28e80411a40
Pablo de la Peña <p@shedcollective.org>
2020-04-27 13:04:16 +0100

    Adds content to the Store class, connecting to the DB on instantiation

--- src/Factory/Store.php
+++ src/Factory/Store.php
@@ -2,6 +2,10 @@
 
 namespace Nails\RelatedContent\Factory;
 
+use HelloPablo\RelatedContent\Exception\MissingExtension;
+use HelloPablo\RelatedContent\Store\MySQL;
+use Nails\Config;
+
 /**
  * Class Store
  *
@@ -9,5 +13,37 @@ namespace Nails\RelatedContent\Factory;
  */
 class Store
 {
+    /** @var MySQL */
+    protected $oStore;
 
+    // --------------------------------------------------------------------------
+
+    /**
+     * Store constructor.
+     *
+     * @throws MissingExtension
+     */
+    public function __construct()
+    {
+        $this->oStore = new MySQL([
+            'host'     => Config::get('DB_HOST'),
+            'user'     => Config::get('DB_USERNAME'),
+            'pass'     => Config::get('DB_PASSWORD'),
+            'database' => Config::get('DB_DATABASE'),
+            'port'     => Config::get('DB_PORT'),
+            'table'    => NAILS_DB_PREFIX . 'related_content_data',
+        ]);
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Returns the instance of the store
+     *
+     * @return \HelloPablo\RelatedContent\Interfaces\Store
+     */
+    public function getStore(): \HelloPablo\RelatedContent\Interfaces\Store
+    {
+        return $this->oStore;
+    }
 }
